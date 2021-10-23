8977e7497ca395003ee0e49cc55b9415af2d95ba
Owullu Amarachukwu <owuluamara@gmail.com>
2021-10-23 15:48:36 +0100

    i added applications and extension in the dashborad for hall admin.

---
+++ hall_admin/Extensions.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include('inc/header.php'); ?>
+
+<body>
+    <div class="container-scroller">
+        <!-- partial:partials/_navbar.html -->
+        <?php include('../partials/_navbar.html'); ?>
+        <!-- partial -->
+        <div class="../container-fluid page-body-wrapper">
+            <!-- partial:partials/_sidebar.html -->
+            <?php include('inc/_sidebar.php'); ?>
+            <!-- partial -->
+            <div class="main-panel">
+                <div class="content-wrapper">
+                    <div class="row">
+                        <div class="col-lg-12 grid-margin stretch-card">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h4 class="card-title">Exeat Extensions</h4>
+                                    <div class="table-responsive">
+                                        <table class="table table-striped">
+                                            <thead>
+                                                <tr>
+                                                    <th>
+                                                        S/N
+                                                    </th>
+                                                    <th>
+                                                        Application Date
+                                                    </th>
+                                                    <th>
+                                                        Destination
+                                                    </th>
+                                                    <th>
+                                                        Leave Type
+                                                    </th>
+                                                    <th>
+                                                        Departure Date
+                                                    </th>
+                                                    <th>
+                                                        Return Date
+                                                    </th>
+                                                    <th>
+                                                        Reason
+                                                    </th>
+                                                    <th>
+                                                        Status
+                                                    </th>
+                                                    <th>
+                                                        Action
+                                                    </th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td>
+                                                        1
+                                                    </td>
+                                                    <td>
+                                                        06-10-2021
+                                                    </td>
+                                                    <td>
+                                                        Lagos
+                                                    </td>
+                                                    <td>
+                                                        Long
+                                                    </td>
+                                                    <td>
+                                                        10-10-2021
+                                                    </td>
+                                                    <td>
+                                                        21-10-2021
+                                                    </td>
+                                                    <td>
+                                                        Lorem ipsum.
+                                                    </td>
+                                                    <td>
+                                                        <span class="badge badge-warning">Pending</span>
+                                                    </td>
+                                                    <td>
+                                                        <a href="viewExtension" class="btn btn-primary">View</a>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- content-wrapper ends -->
+                <!-- partial:partials/_footer.html -->
+                <footer class="footer">
+                    <div class="d-sm-flex justify-content-center justify-content-sm-between">
+                        <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright ©
+                            2021.</span>
+                    </div>
+                    <div class="d-sm-flex justify-content-center justify-content-sm-between">
+                        <!-- <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Distributed by <a href="https://www.themewagon.com/" target="_blank">Themewagon</a></span>  -->
+                    </div>
+                </footer>
+                <!-- partial -->
+            </div>
+            <!-- main-panel ends -->
+        </div>
+        <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+
+    <?php include('inc/footer.php'); ?>
+</body>
+
+</html>
